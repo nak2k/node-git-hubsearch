22a48ef8492c67c2839bec0f2c5065f8260d368e
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2016-02-23 06:04:38 +0900

    Colorize with chalk

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var Github = require('github-api');
 var columnify = require('columnify');
 var debug = require('debug')('git-hubsearch');
 var inspect = require('util').inspect;
+var chalk = require('chalk');
 
 var yargs = require('yargs')
   .boolean(['help', 'fork'])
@@ -54,15 +55,24 @@ module.exports = function() {
       })
       .sort(function(a, b) {
         return a.name > b.name ? 1 : a.name === b.name ? 0 : -1;
+      })
+      .map(function(repo) {
+        repo.homepage = repo.homepage || repo.html_url;
+        repo.pushed_at = new Date(repo.pushed_at).toISOString().split('T')[0];
+        return repo;
       });
 
     var output = columnify(repos, {
-      include: ["name", "description"],
+      include: ["name", "description", "pushed_at", "homepage"],
       truncate: false,
       config: {
         name: { maxWidth: 40, truncate: false, truncateMarker: '' },
         description: { },
       }
+    });
+
+    criteria.forEach(function(word) {
+      output = output.replace(new RegExp(word, 'gim'), chalk.red(word));
     });
 
     console.log(output);
