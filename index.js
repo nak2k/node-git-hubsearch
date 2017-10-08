ff74f0f8c831fcec8bf6897a9463da4e5e50b2b1
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2017-10-08 21:04:57 +0900

    Show private repos

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const chalk = require('chalk');
 const yargs = require('yargs')
   .boolean(['help', 'fork'])
   .default({
-    user: process.env.USER,
     fork: false,
   })
   .alias({
@@ -23,7 +22,9 @@ module.exports = () => {
   const argv = yargs.argv;
   debug(inspect(argv));
 
-  const github = new Github({});
+  const github = new Github({
+    token: process.env.GITHUB_TOKEN,
+  });
 
   const criteria = argv._;
   const user = github.getUser(argv.user);
@@ -40,8 +41,17 @@ module.exports = () => {
           (repo.description && repo.description.indexOf(word) >= 0)))
       .sort((a, b) => a.name > b.name ? 1 : a.name === b.name ? 0 : -1)
       .map(repo => {
+        repo.description = repo.description || '';
         repo.homepage = repo.homepage || repo.html_url;
         repo.pushed_at = new Date(repo.pushed_at).toISOString().split('T')[0];
+
+        if (repo.private) {
+          repo.name = chalk.gray(repo.name);
+          repo.description = chalk.gray(repo.description);
+          repo.pushed_at = chalk.gray(repo.pushed_at);
+          repo.homepage = chalk.gray(repo.homepage);
+        }
+
         return repo;
       });
 
